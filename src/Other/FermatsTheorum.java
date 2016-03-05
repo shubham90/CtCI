34970d9d4901e3f7f9f64a63ac4b017ac22d4e34
Shubham <shubham.bhawsinka@gmail.com>
2016-03-04 19:40:00 -0500

    Added comments

--- src/Other/FermatsTheorum.java
+++ src/Other/FermatsTheorum.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* Finding if a number is prime or not. */
 
 package Other;
